45f6f22f3f86c8e119dd6320038a9df6999d4fb6
Patrick Melo <patrickabimael@yahoo.com.br>
2024-08-24 22:38:39 -0300

    resolve desafio do iPhone

---
+++ logica/src/iphone/iPhone.java
@@ -0,0 +1,48 @@
+package iphone;
+
+public class iPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
